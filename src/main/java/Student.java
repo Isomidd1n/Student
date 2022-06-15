19db7352546e5e339ade96e4dce30f554d339e39
Isomiddin <isomiddinhusniddinov21@gmail.com>
2022-06-15 11:21:06 +0500

    Student fanlari methodi.

---
+++ src/main/java/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    String name;
+    String lastName;
+    int groupNumber;
+    String lessons;
+
+   public void printSubjects(){
+//        lessons.equals("1.Informatika");
+//        lessons.equals("2.Ingliz tili");
+//        lessons.equals("3.Pc science");
+//        lessons.equals("4.Kibersport");
+//        lessons.equals("5.Kiber Security");
+        System.out.println("1.Informatika");
+        System.out.println("2.Ingliz tili");
+        System.out.println("3.Pc science");
+        System.out.println("4.Kibersport");
+        System.out.println("5.Kiber Security");
+    }
+
+}
